ed03897bc347db2250c898a85cb5b406b7ebf33b
Maji <start99start@gmail.com>
2023-03-18 13:33:16 +0800

    add logout

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,10 +3,33 @@ const express = require('express')
 const router = express.Router()
 // 引用 User model
 const User = require('../../models/user')
+const cookieParser = require('cookie-parser');
+router.use(cookieParser());
 
 // 設定路由：首頁
 router.get('/', (req, res) => {
-  res.render ('index')
+  //若從cookie可辨認出user身分，則無需再請user輸入帳密登入
+  User.findOne({ firstName: Object.keys(req.cookies) })
+    .lean()
+    .then(user => {
+      if (user) {
+        res.render('show', { firstName: user.firstName })
+      } else {
+        res.render('index')
+      }
+    })
+})
+
+//若user選擇登出，則將user的cookie標示移除，並返回首頁
+router.get('/logout', (req, res) => {
+  User.findOne({ firstName: Object.keys(req.cookies) })
+    .lean()
+    .then(user => {
+      if (user) {
+        res.clearCookie(user.firstName)
+        res.redirect('/')
+      }
+    })
 })
 
 // 匯出路由模組
--- routes/modules/login.js
+++ routes/modules/login.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 // 引用 User model
 const User = require('../../models/user')
+const cookieParser = require('cookie-parser');
+router.use(cookieParser());
 
 //設定路由：接住輸入之帳密
 router.post('/', (req, res) => {
@@ -13,15 +15,19 @@ router.post('/', (req, res) => {
     .lean()
     .then(user => {
       if (user) {
+        //登入後, 先清除之前的cookie
+        for (const key in req.cookies) { res.clearCookie(key) }
         res.cookie(user.firstName, user._id) //若登入成功, 植入cookie
         return res.render('show', { firstName: user.firstName })
+
       } else {
-        User.findOne({ email })
+
+        User.findOne({ email })  //若帳密比對失敗，確認是否為信箱對密碼錯
           .lean()
           .then(user => {
             if (user) {
               return res.render('passwordwrong')
-            } else {
+            } else {              //若信箱錯誤，則顯示信箱不存在
               return res.render('notfound', { email })
             }
           })
